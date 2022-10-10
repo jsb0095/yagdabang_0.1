bdf4ac01116cc38aa4164a650189443af7ca5ee8
jsb0095 <anfzhd0095@gmail.com>
2022-10-10 18:45:16 +0900

    피드 매인화면 제작 완료 버튼 기능들 미구현

--- lib/components/avatar_widget.dart
+++ lib/components/avatar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 enum AvatarType { type1, type2, type3 }
 
@@ -36,18 +37,52 @@ class AvatarWidget extends StatelessWidget {
           shape: BoxShape.circle),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(65),
-        child: Container(
-          width: 65,
-          height: 65,
-          child: CachedNetworkImage(
-            imageUrl: thumPath,
-            fit: BoxFit.cover,
-          ),
+        child: type2Widget()));
+  }
+  Widget type2Widget(){
+   return Container(
+
+     width: 75,height: 75,
+      padding: const EdgeInsets.all(2),
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color:Colors.white
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(65),
+        child: CachedNetworkImage(
+          imageUrl: thumPath,
+          fit: BoxFit.cover,
         ),
       ),
     );
   }
 
+  Widget type3Widget(){
+    return Row(
+      children: [
+        Container(
+        width: 40,
+        height: 40,
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color(0xffff9000),
+                Color(0xffac11ff),
+              ],
+            ),
+            shape: BoxShape.circle),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(65),
+           )),
+        Text(nickname ?? '',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 15),)
+      ],
+    );
+  }
+  
   @override
   Widget build(BuildContext context) {
     switch (type) {
@@ -55,8 +90,9 @@ class AvatarWidget extends StatelessWidget {
         return type1Widget();
         break;
       case AvatarType.type2:
+        return type2Widget();
       case AvatarType.type3:
-        return Container();
+        return type3Widget();
         break;
     }
     return Container();
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,26 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:yagdabang/components/avatar_widget.dart';
+import 'package:yagdabang/components/post_widget.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
+
+  Widget _myStory(){
+    return Stack(
+      children: [
+        AvatarWidget(thumPath:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6gBc7B6Wfhcl58kElznvZlIDXrntcKIHXcw&usqp=CAU', type: AvatarType.type2
+        ),
+        Positioned(right: 5,bottom: 0,child: Container(
+          width: 25,
+          height: 25,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.blue,
+            border: Border.all(color: Colors.white,width: 2)
+          ),
+          child: const Center(
+            child: Text("+"),
+          ),
+        ),)
+      ],
+    );
+  }
 
   Widget _storyBoardList() {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: List.generate(
-          100,
-          (index) => AvatarWidget(
-            type: AvatarType.type1,
-            thumPath:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUuCc7h1v7jPfjqsDgjkW1IxFIgYhTcjubdA&usqp=CAU',
-          ),
-        ),
-      ),
+        children:[
+          const SizedBox(width: 10),
+          _myStory(),
+        const SizedBox(width: 10),
+        ...List.generate(
+        100,
+        (index) => AvatarWidget(
+      type: AvatarType.type1,
+      thumPath:
+       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUuCc7h1v7jPfjqsDgjkW1IxFIgYhTcjubdA&usqp=CAU',
+    ),
+    ),
+    ] ),
     );
   }
 
+  Widget _postList(){
+    return Column(
+      children:
+        List.generate(50, (index) => PostWidget()).toList(),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,7 +77,8 @@ class Home extends StatelessWidget {
         ],
       ),
       body: ListView(
-        children: [_storyBoardList()],
+        children: [_storyBoardList(),
+        _postList(),],
       ),
     );
   }
